2d02a02f1b54da106a5d0600bcc98a361a063834
Sepiac <danerussell4@gmail.com>
2016-01-07 18:48:25 -0500

    Now accepts any remote code on startup to toggle between the "on" and "motion" states.

--- motion_module_with_ir.ino
+++ motion_module_with_ir.ino
@@ -1,53 +1,59 @@
 #include <IRLib.h>
+#define MOTION_MODE 0
+#define OFF_MODE 1
+#define ON_MODE 2
+
 const int externalDevicePin = 3;
 const int remotePin = 4;
 const int motionPin = 5;
+const int ledIndicatorPin = 13;
 
+unsigned long remoteCode = 0;
 unsigned long lastMotionTime;
 unsigned long shutoffDuration = 60000 * 5;
-/*
-*0: default behavior (turn light on with motion for 5 minutes aka automatic mode)
-*1: leave light off
-*2: leave light on
-*/
-//the indicator pin is low when automatic mode is ON and high when automatic mode is OFF
-const int automaticModeIndicatorPin = 13;
-int detectionMode = 0;
+
+int detectionMode = MOTION_MODE;
+
 IRrecv receiver(remotePin);
 IRdecode decoder;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(externalDevicePin, OUTPUT);
-  pinMode(automaticModeIndicatorPin, OUTPUT);
   pinMode(motionPin, INPUT);
   receiver.enableIRIn();
+
+  //scan for initial remote code
+  while (remoteCode == 0) {
+    if(receiver.GetResults(&decoder)) {
+      decoder.decode();
+      remoteCode = decoder.value;
+      receiver.resume();
+    }
+    
+    digitalWrite(ledIndicatorPin, millis()%1000 < 500);
+  }
+
+  digitalWrite(ledIndicatorPin, LOW);
 }
 
 void loop() {
 
   if(receiver.GetResults(&decoder)) {
     decoder.decode();
-    switch(decoder.value) {
-      case 0x20DF8877:
-        detectionMode = 0;
-        digitalWrite(automaticModeIndicatorPin, LOW);
-        break;
-      case 0x20DF48B7:
-        detectionMode = 1;
-         digitalWrite(automaticModeIndicatorPin, HIGH);
-        break;
-      case 0x20DFC837:
-        detectionMode = 2;
-         digitalWrite(automaticModeIndicatorPin, HIGH);
-        break;
+    //toggle between motion detection and off
+    if(decoder.value == remoteCode ) {
+      if(detectionMode == MOTION_MODE) {
+        detectionMode = OFF_MODE;
+      } else if(detectionMode == OFF_MODE) {
+        detectionMode = MOTION_MODE;
+      }
     }
 
     receiver.resume();
   }
 
   switch(detectionMode) {
-    case 0:
+    case MOTION_MODE:
       if(digitalRead(motionPin)) {
         lastMotionTime = millis();
         digitalWrite(externalDevicePin, HIGH);
@@ -55,10 +61,10 @@ void loop() {
         digitalWrite(externalDevicePin, LOW);
       }
       break;
-    case 1:
+    case OFF_MODE:
       digitalWrite(externalDevicePin, LOW);
       break;
-    case 2:
+    case ON_MODE:
     digitalWrite(externalDevicePin, HIGH);
       break;
   }
